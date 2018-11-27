66a158e4e31adde1d9644af66d27625c2d7846c7
xueshaoqi <zhangliangdong@hzzl.net>
2018-11-27 18:15:02 +0800

    'xxx'

--- src/pages/setAPI/index.vue
+++ src/pages/setAPI/index.vue
@@ -3,6 +3,22 @@
 import './index.less';
 export default {
   data () {
+    let validateDelayTime = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('二维码过期时间不能为空'));
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error('请输入数字值'));
+        } else {
+          if (value < 120 || value > 600) {
+            callback(new Error('过期时间必须设置在120-600秒之间'));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
+    };
     return {
       labelPosition: 'right',
       formLabelAlign4: {
@@ -11,6 +27,7 @@ export default {
       rules4: {
         delayTime: [
           { required: true, message: '二维码过期时间不能为空'},
+          { type: 'number', message: '二维码过期时间必须为数字值'}
         ]
       }
     };
