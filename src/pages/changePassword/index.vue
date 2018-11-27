c6b57f0160a823dc228f758ada735431218c0931
xueshaoqi <zhangliangdong@hzzl.net>
2018-11-27 11:55:23 +0800

    'xx'

--- src/pages/changePassword/index.vue
+++ src/pages/changePassword/index.vue
@@ -3,12 +3,43 @@
 import './index.less';
 export default {
   data () {
+    let validateOldPass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入旧密码'));
+      } else {
+        callback();
+      }
+    };
+
+    let validateNewPass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入新密码'));
+      } else {
+        if (this.formLabelAlign.reinput !== '') {
+          this.$refs.formLabelAlign.validateField('reinput');
+        }
+        callback();
+      }
+    };
+
+    let validateReinput = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入新密码'));
+      } else if (value !== this.formLabelAlign.newPassword) {
+        callback(new Error('两次输入密码不一致!'));
+      } else {
+        callback();
+      }
+    };
     return {
       labelPosition: 'right',
       formLabelAlign: {
         oldPassword: '',
         newPassword: '',
         reinput: ''
+      },
+      rules: {
+
       }
     };
   },
