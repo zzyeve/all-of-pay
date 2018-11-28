f1d92749977be778e203e4fe52124f3c5d34f049
xueshaoqi <zhangliangdong@hzzl.net>
2018-11-28 21:55:27 +0800

    'xxx'

--- src/pages/myPackage/index.vue
+++ src/pages/myPackage/index.vue
@@ -44,16 +44,65 @@ export default {
             label: '专业版套餐'
           }
         ]
+      },
+      packageInfo: {
+        packageName: '',
+        packageId: 'AOP_5fb32426aeb24e5aa71627dd9294193d',
+        packagePrice: '',
+        startTime: '',
+        endTime: ''
       }
     };
   },
   created () {
-    // this.getData();
+    this.getUserPackageInformation();
+    this.queryPackageInfo();
   },
-  metods: {
-    // getData () {
-      // console.log('1');
-    // }
+  methods: {
+    // 请求用户套餐信息查询接口
+    getUserPackageInformation () {
+      let params = {
+        apiUid: 'AOP_5fb32426aeb24e5aa71627dd9294193d'
+      };
+      let string = JSON.stringify(params);
+      this.$api.getUserPackageInfo(string).then(res => {
+        console.log(res.packageId);
+        if (res.packageName === '') {
+          this.havePurchase = false;
+          this.notPurchase = true;
+        } else {
+          this.havePurchase = true;
+          this.notPurchase = false;
+        }
+        this.packageInfo.packageName = res.packageName;
+        this.packageInfo.packageId = res.packageId;
+        this.packageInfo.packagePrice = res.packagePrice;
+        this.packageInfo.startTime = res.packageSubscriptTime;
+        this.packageInfo.endTime = res.packageExpiryTime;
+      });
+    },
+
+    // 请求套餐信息查询接口
+    queryPackageInfo () {
+      let params = {
+        packageId: this.packageInfo.packageId
+      };
+      let string = JSON.stringify(params);
+      this.$api.getPackageInfo(string).then(res => {
+        console.log(res);
+      });
+    },
+
+    // 请求用户余额明细查询接口
+    getBalanceDetail () {
+      let params = {
+        apiUid: 'AOP_5fb32426aeb24e5aa71627dd9294193d'
+      };
+      let string = JSON.stringify(params);
+      this.$api.getBalanceDetail(string).then(res => {
+        console.log(res);
+      });
+    }
   }
 };
 </script>
